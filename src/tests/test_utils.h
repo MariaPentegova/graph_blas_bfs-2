54fa4d18138b0b5316c46497e372922492e2978c
MariaPentegova <m.pentegova@yandex.ru>
2026-08-24 23:26:20 +0300

    Create test_utils.h

---
+++ src/tests/test_utils.h
@@ -0,0 +1,12 @@
+#ifndef TEST_UTILS_H
+#define TEST_UTILS_H
+
+Graph* create_test_graph();
+
+CSRMatrix* create_test_csr();
+
+int check_all_visited(int* level, int n);
+int compare_levels(int* level1, int* level2, int n);
+int check_parent_correctness(int* parent, int n, int start_vertex);
+
+#endif
