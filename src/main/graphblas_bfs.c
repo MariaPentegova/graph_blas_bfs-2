36bb20e6f74b27655f11b8c83bfb6efd69b7d4c0
MariaPentegova <m.pentegova@yandex.ru>
2026-08-24 14:03:42 +0300

    Update graphblas_bfs.c

--- src/main/graphblas_bfs.c
+++ src/main/graphblas_bfs.c
@@ -37,8 +37,8 @@ int graphblas_level_bfs(CSRMatrix* csr, int start_vertex, int* level) {
     
     int n = csr->n;
     GrB_Matrix A = NULL;
-    if (import_csr_to_graphblas(csr, &A) != 0) {
-        printf("Ошибка: не удалось импортировать CSR в GraphBLAS\n");
+    if (csr_to_graphblas(csr, &A) != 0) {
+        printf("Error: не удалось превратить CSR в GraphBLAS\n");
         return -1;
     }
     
@@ -70,6 +70,98 @@ int graphblas_level_bfs(CSRMatrix* csr, int start_vertex, int* level) {
             visited,                      
             NULL,                         
             GrB_LOR_LAND_BOOL_SEMIRING,  
+            A,                            
+            frontier,                     
+            NULL                          
+        );
+        
+        GrB_eWiseAdd(
+            visited,
+            NULL,
+            NULL,
+            GrB_LOR_BOOL,
+            visited,
+            new_frontier,
+            NULL
+        );
+        
+        GrB_assign(
+            level_vec,
+            new_frontier,                 
+            NULL,
+            current_level + 1,
+            GrB_ALL,
+            n,
+            NULL
+        );
+        
+        GrB_Vector_clear(frontier);
+        GrB_assign(frontier, NULL, NULL, new_frontier, GrB_ALL, n, NULL);
+        
+        GrB_Vector_nvals(&nvals, new_frontier);
+        current_level++;
+    }
+    
+    for (int i = 0; i < n; i++) {
+        GrB_Vector_extractElement_INT32(&level[i], level_vec, i);
+    }
+    
+    GrB_Matrix_free(&A);
+    GrB_Vector_free(&level_vec);
+    GrB_Vector_free(&frontier);
+    GrB_Vector_free(&visited);
+    GrB_Vector_free(&new_frontier);
+    
+    return 0;
+}
+
+// Multisource Level BFS на GraphBLAS
+int graphblas_multisource_level_bfs(CSRMatrix* csr, int* sources, int num_sources, int* level) {
+    if (csr == NULL || level == NULL || sources == NULL || num_sources <= 0) {
+        return -1;
+    }
+    
+    int n = csr->n;
+    
+    GrB_Matrix A = NULL;
+    if (csr_to_graphblas(csr, &A) != 0) {
+        printf("Errro: не удалось импортировать CSR в GraphBLAS\n");
+        return -1;
+    }
+    
+    GrB_Vector level_vec = NULL;
+    GrB_Vector frontier = NULL;
+    GrB_Vector visited = NULL;
+    GrB_Vector new_frontier = NULL;
+    
+    GrB_Vector_new(&level_vec, GrB_INT32, n);
+    GrB_Vector_new(&frontier, GrB_BOOL, n);
+    GrB_Vector_new(&visited, GrB_BOOL, n);
+    GrB_Vector_new(&new_frontier, GrB_BOOL, n);
+    
+    for (int i = 0; i < n; i++) {
+        level[i] = -1;
+    }
+    
+    for (int i = 0; i < num_sources; i++) {
+        int s = sources[i];
+        if (s >= 0 && s < n) {
+            GrB_Vector_setElement_INT32(level_vec, 0, s);
+            GrB_Vector_setElement_BOOL(frontier, 1, s);
+            GrB_Vector_setElement_BOOL(visited, 1, s);
+            level[s] = 0;
+        }
+    }
+    
+    int current_level = 0;
+    GrB_Index nvals = num_sources;  
+    
+    while (nvals > 0) {
+        GrB_mxv(
+            new_frontier,                 
+            visited,                      
+            NULL,                         
+            GrB_LOR_LAND_BOOL_SEMIRING,   
             A,                            
             frontier,                     
             NULL                          
