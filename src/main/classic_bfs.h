63baf2c7b47f2de14c5b1588ced9aeaa8d7e799c
MariaPentegova <m.pentegova@yandex.ru>
2026-08-24 11:21:59 +0300

    Update classic_bfs.h

--- src/main/classic_bfs.h
+++ src/main/classic_bfs.h
@@ -2,6 +2,6 @@
 #define CLASSIC_BFS_H
 
 void csr_parent_bfs(CSRMatrix* csr, int start_vertex, int* parent)
-
+void csr_multisource_bfs(CSRMatrix* csr, int* sources, int num_sources, int* parent)
 
 #endif
