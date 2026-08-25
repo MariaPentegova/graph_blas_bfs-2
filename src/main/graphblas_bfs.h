e0c939e71a19f3a2af333ba74af578b6dcef91aa
MariaPentegova <m.pentegova@yandex.ru>
2026-08-25 22:47:20 +0300

    Update graphblas_bfs.h

--- src/main/graphblas_bfs.h
+++ src/main/graphblas_bfs.h
@@ -6,7 +6,7 @@
 #include <GraphBLAS.h>   
 #include "utils.h"
 
-csr_to_graphblas(CSRMatrix* csr, GrB_Matrix* A);
+int csr_to_graphblas(CSRMatrix* csr, GrB_Matrix* A);
 int graphblas_level_bfs(CSRMatrix* csr, int start_vertex, int* level);
 int graphblas_multisource_level_bfs(CSRMatrix* csr, int* sources, int num_sources, int* level);
 
