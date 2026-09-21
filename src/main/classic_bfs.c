d9f0c2b573473b2f1a59fca9ae32e0cbccb73788
MariaPentegova <m.pentegova@yandex.ru>
2026-09-21 14:05:52 +0300

    Update classic_bfs.c

--- src/main/classic_bfs.c
+++ src/main/classic_bfs.c
@@ -11,11 +11,11 @@ void csr_parent_bfs(CSRMatrix* csr, int start_vertex, int* parent) {
     for (int i = 0; i < n; i++) {
         parent[i] = -1; 
     }
-    parent[start_vertex] = start_vertex;  // корень указывает на себя
+    parent[start_vertex] = start_vertex; 
     
     int* queue = (int*)malloc(n * sizeof(int));
     if (queue == NULL) {
-        printf("Error: не удалось выделить память для очереди\n");
+        printf("Error: failed to allocate memory for the queue\n");
         return;
     }
     
@@ -55,7 +55,7 @@ void csr_multisource_bfs(CSRMatrix* csr, int* sources, int num_sources, int* par
     
     int* queue = (int*)malloc(n * sizeof(int));
     if (queue == NULL) {
-        printf("Error: не удалось выделить память для очереди\n");
+        printf("Error: failed to allocate memory for the queue\n");
         return;
     }
     
